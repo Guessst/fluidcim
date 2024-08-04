b8771e1bfe21cf77b2fef0e9db00ecb25a77495e
Guessst <gustavohqueiroz22@gmail.com>
2024-08-04 11:18:14 -0300

    corrected some things, drating honey honey

--- src/constants.h
+++ src/constants.h
@@ -2,5 +2,8 @@
 #define CONSTANTS_H_
 
 #define N 100
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 800
+#define MIN(X, Y) X > Y ? Y : X
 
 #endif
--- src/main.c
+++ src/main.c
@@ -5,57 +5,56 @@
 #include "rlgl.h"
 #include "raymath.h"
 
-// #include <omp.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-const float WATER_VISC = 0.89;
-const float WATER_DENS = 0.997;
+#define WATER_VISC 0.89
+#define WATER_DENS 0.997
 
+#define HONEY_VISC 10000*WATER_VISC
+#define HONEY_DENS 1.4*WATER_DENS
+
+unsigned char flagWaterHoney = 0;
+
+const int minScreenDimension = MIN(SCREEN_WIDTH, SCREEN_HEIGHT);
+const int squareSize = (int)(1.0f * ((float)minScreenDimension / (float)N));
+const int squarePadding = (int)(0.3f * ((float)minScreenDimension / (float)N));
 unsigned char squareColored[N*N] = {0};
-unsigned int currDebugs;
 
-void startDebug()
+void drawGridElement(int i, int j, Color color)
 {
-    currDebugs = 0;
+
+    float posX =  i*squareSize + i*squarePadding;
+    float posY =  j*squareSize + j*squarePadding;
+
+    DrawRectangle(
+        posX,
+        posY,
+        squareSize,
+        squareSize,
+        color
+        // (Color){ 255, 255, 255, 255*(densAtPos / 0.1)}
+    );
 }
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 int main(void)
 {
 
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 800;
-    const int min = screenHeight < screenWidth ? screenHeight : screenWidth;
-
-    const int squareSize = (int)(1.0f * ((float)min / (float)N));
-    const int squarePadding = (int)(0.3f * ((float)min / (float)N));
 
     const float initialZoom = 0.5f;
-    // const int squarePadding = 100;
 
-    InitWindow(screenWidth, screenHeight, "fluidcim");
-
-    // Image screen = LoadImageFromScreen();
-    // ImageDrawRectangle(&screen, 0, 0, screen.width, screen.height, RED);
-
-    // Texture2D texture = LoadTextureFromImage(parrots);      // Image converted to texture, uploaded to GPU memory (VRAM)
-    // UnloadImage(parrots);   // Once image has been converted to texture and uploaded to VRAM, it can be unloaded from RAM
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "fluidcim");
 
     SetTargetFPS(60);
     
     Camera2D camera = { 0 };
-    // TODO: calcular offset
-    camera.offset = (Vector2){ screenWidth/6.0f, screenHeight/6.0f };
+    // TODO: properly compute offset
+    camera.offset = (Vector2){ SCREEN_WIDTH/6.0f, SCREEN_HEIGHT/6.0f };
     camera.zoom = initialZoom;
     //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose())        // Detect window close button or ESC key
+    while (!WindowShouldClose())
     {
         // Update
         //----------------------------------------------------------------------------------
@@ -87,18 +86,21 @@ int main(void)
             camera.zoom += (wheel*zoomIncrement);
             if (camera.zoom < zoomIncrement) camera.zoom = zoomIncrement;
         }
-
+        // Reset simulation
         if(IsKeyPressed(KEY_SPACE))
         {
             camera.target = Vector2Zero();
-            camera.offset = (Vector2){ screenWidth/6.0f, screenHeight/6.0f };
+            camera.offset = (Vector2){ SCREEN_WIDTH/6.0f, SCREEN_HEIGHT/6.0f };
             camera.zoom = initialZoom;
-            for(int i = 0;i < N;i++)
+            
+            for(int i = 0;i < sizeOfBuffer;i++)
             {
-                for(int j = 0;j < N;j++)
-                {
-                    dens_prev[IX(i, j)] = 0;
-                }
+                u[i] = 0;
+                v[i] = 0;
+                u_prev[i] = 0;
+                v_prev[i] = 0;
+                dens[i] = 0;
+                dens_prev[i] = 0;
             }
         }
         if(IsMouseButtonDown(MOUSE_BUTTON_LEFT))
@@ -111,10 +113,10 @@ int main(void)
                     float posY =  j*squareSize + j*squarePadding;
                     if(Vector2Distance(mouseWorldPos, (Vector2){posX, posY}) <= squareSize*10)
                     {
-                        dens_prev[IX(i, j)] = WATER_DENS;
-                        if(Vector2Distance(mouseWorldPos, (Vector2){posX, posY}) == squareSize*10){
-                        squareColored[i*N + j] = 1;
-                        }
+                        dens_prev[IX(i, j)] = (flagWaterHoney ? WATER_DENS : HONEY_DENS);
+                        // if(Vector2Distance(mouseWorldPos, (Vector2){posX, posY}) == squareSize*10){
+                        // squareColored[i*N + j] = 1;
+                        // }
                         // printf("%f %f\n", posX, posY);
                     }
                     else
@@ -124,7 +126,7 @@ int main(void)
                 }
             }
         }
-        if(IsMouseButtonPressed(MOUSE_BUTTON_MIDDLE))
+        if(IsMouseButtonDown(MOUSE_BUTTON_MIDDLE))
         {
             for(int i = 0;i < N;i++)
             {
@@ -139,8 +141,15 @@ int main(void)
                         // dens[IX(i, j)] = 0;
                         // v[IX(i, j)] = 0;
                         // dens_prev[IX(i, j)] = 0;
-                        v_prev[IX(i, j)] = -v_prev[IX(i, j)];
-                        squareColored[i*N + j] = 1;
+                        if(dens_prev[IX(i, j)] > 0) dens_prev[IX(i, j)] = 0;
+                        // u_prev[IX(i, j)] = WATER_VISC;
+                        
+                        
+                        // TODO: modificar u_prev 
+
+
+
+                        // squareColored[i*N + j] = 1;
                         // printf("%f %f\n", posX, posY);
                     }
                     else
@@ -150,6 +159,10 @@ int main(void)
                 }
             }
         }
+        if(IsKeyPressed(KEY_H))
+        {
+            flagWaterHoney = !flagWaterHoney;
+        }
         
 
         //----------------------------------------------------------------------------------
@@ -158,14 +171,6 @@ int main(void)
         //----------------------------------------------------------------------------------
         BeginDrawing();
             ClearBackground(BLACK);
-
-
-            // const char* myStr = (char*)malloc(sizeof(char)*15);
-            // sprintf(myStr, "ThreadNum: %d", omp_get_max_threads());
-            // DrawText(myStr, 0, 0, 10, RAYWHITE);
-
-            // printf("posx %f, posy: %f\n", pos.x, pos.y);
-
             /*
                 TODO
 
@@ -177,44 +182,68 @@ int main(void)
 
             */
 
-            vel_step(u, v, u_prev, v_prev, WATER_VISC*20, GetFrameTime());//GetFrameTime());
-            dens_step(dens, dens_prev, u, v, WATER_DENS, GetFrameTime());//GetFrameTime());
+            // TODO: pausar simulação para debuggar sem usar breakpoint
+            // TODO: fazer struct pra debugar os quadrado
+
+            if(flagWaterHoney)
+            {
+                vel_step(u, v, u_prev, v_prev, WATER_VISC, GetFrameTime());
+                dens_step(dens, dens_prev, u, v, WATER_DENS, GetFrameTime());
+            }
+            else
+            {
+                vel_step(u, v, u_prev, v_prev, HONEY_VISC, GetFrameTime());
+                dens_step(dens, dens_prev, u, v, HONEY_DENS, GetFrameTime());
+            }
 
             BeginMode2D(camera);
-                // #pragma omp parallel for
                 for(int i = 0;i < N;i++)
                 {
                     for(int j = 0;j < N;j++)
                     {
+                            // if((i + j)%2 == 0) u_prev[IX(i, j)] = 10;
 
-                        float posX =  i*squareSize + i*squarePadding;
-                        float posY =  j*squareSize + j*squarePadding;
-                        // if(
-                        //     pos.x <= posX && posX <= pos.x + squarePadding
-                        //     && pos.y <= posY && posY <= pos.y + squarePadding
-                        // )
-                        // {
                             float densAtPos = dens[IX(i, j)];
-
-                            DrawRectangle(
-                                posX,
-                                posY,
-                                squareSize,
-                                squareSize,
-                                squareColored[i*N + j] ? RED : (
-                                    (densAtPos / WATER_DENS) <= 1.0 ?
-                                        ((Color){ 255, 255, 255, 255*(densAtPos / WATER_DENS) })
-                                        : BLUE
-                                )
-                                // (Color){ 255, 255, 255, 255*(densAtPos / 0.1)}
-                            );
-                        // }
+                            float uPrev = u_prev[IX(i, j)];
+                            // float vAtPos = v[IX(i, j)];
+                            if(flagWaterHoney)
+                            {
+                                drawGridElement(
+                                    i,
+                                    j,
+                                    squareColored[i*N + j] ?
+                                        RED 
+                                        : (
+                                            (densAtPos / WATER_DENS) <= 1.0 ?
+                                                ((Color){ 255, 255, 255, 255*(densAtPos / WATER_DENS) })
+                                                : BLUE
+                                        )
+                                );
+                            }
+                            else
+                            {
+                                drawGridElement(
+                                    i,
+                                    j,
+                                    squareColored[i*N + j] ?
+                                        RED 
+                                        : (
+                                            (densAtPos / HONEY_DENS) <= 1.0 ?
+                                                ((Color){ 255, 255, 255, 255*(densAtPos / HONEY_DENS) })
+                                                : BLUE
+                                        )
+                                );
+                            }
+                            const Color transparentOrange = (Color){ 255, 161, 0, 255/2 };
+                            // if(uPrev >= WATER_VISC) drawGridElement(i, j, ORANGE);
                     }
                 }
             EndMode2D();
         
         DrawFPS(0, 0);
-        DrawText(TextFormat("%.3fms", (1.0f / GetFPS())), 0, 100, 20, RED);
+        DrawText(TextFormat("%fms", GetFrameTime()*1000.0f), 0, 100, 20, RED);
+        if(flagWaterHoney) {DrawText("WATER", 0, 200, 20, BLUE);}
+        else {DrawText("honey", 0, 200, 20, YELLOW);}
         
         EndDrawing();
         //----------------------------------------------------------------------------------
